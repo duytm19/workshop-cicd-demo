091b738c4ef7c6e12a2cc1ad306297fc981c6ddf
duytm19 <tranminhduy1912@gmail.com>
2025-11-21 13:42:54 +0700

    feat: add

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,7 @@
 const sum = require('./sum');
 
-test('adds 3 + 5 to equal 8', () => {
-  expect(sum(3, 5)).toBe(8);
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
 
 test('adds 0 + 0 to equal 0', () => {
